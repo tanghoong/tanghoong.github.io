c5cdd585b4399f40bae78ff4dbc6a05bb099b33f
cth-inni <charlie.tang@innity.com>
2021-08-15 01:42:08 +0800

    Chat sample

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <ul>
+        <li v-for="user in users" :key="user.id"> {{user.name}} </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+        import chatData from "./sample-chat-data.json";
+        export default {
+            data() {
+                return {
+                    users: chatData,
+                };
+            },
+        };
+</script>
---
+++ todo.md
@@ -0,0 +1,18 @@
+---
+John: red,image_link
+Doe: green,image_link
+---
+
+- [] markdown to json
+- [] system dialog in middle (italic text, image)
+- [] show avatar on top of dialog
+- [] show name on top of dialog
+- [] color at dialog box
+- [] 
+- [] 
+
+
+
+John: Hello
+
+Doe: is?
